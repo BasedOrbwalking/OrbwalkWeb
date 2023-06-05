967a943eed4e40fe769caadee7db303cad3cf04e
expo <expo@prettybased.tech>
2023-06-05 20:57:42 +0200

    troll

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
+        /*
         'primary': {
           DEFAULT: '#6366F1',
           50: '#FFFFFF',
@@ -21,6 +22,19 @@ module.exports = {
           700: '#1317D1',
           800: '#0E119E',
           900: '#0A0C6A'
+        },*/
+        'primary': {
+          DEFAULT: '#6366F1',
+          50: '#f5f3ff',
+          100: '#ede9fe',
+          200: '#ddd6fe',
+          300: '#c4b5fd',
+          400: '#a78bfa',
+          500: '#8b5cf6',
+          600: '#7c3aed',
+          700: '#6d28d9',
+          800: '#5b21b6',
+          900: '#4c1d95'
         },
         'secondary': {
           DEFAULT: '#EC4899',
